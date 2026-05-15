29c613b2b2fe38e950513d63ad65010a59e7d75b
Gerald Morales <85576040+geraldmrls@users.noreply.github.com>
2026-05-14 23:02:37 -0600

    with edge-to-edge enabled.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,20 @@
 import { AuthContext } from "@/lib/AuthContext";
+import * as NavigationBar from "expo-navigation-bar";
 import { Stack } from "expo-router";
-import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   const [isLogIn, setIsLogIn] = useState<boolean>(false);
-
+  useEffect(() => {
+    NavigationBar.setBackgroundColorAsync("#eef0f7");
+    NavigationBar.setButtonStyleAsync("dark");
+  }, []);
   return (
     <SafeAreaProvider>
       <AuthContext.Provider value={{ isLogIn, setIsLogIn }}>
+        <StatusBar style="dark" backgroundColor="#eef0f7" />
         <Stack screenOptions={{ headerShown: false }} />
       </AuthContext.Provider>
     </SafeAreaProvider>
